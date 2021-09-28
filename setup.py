17690815e5e052e0419fadebf35a8a84b6593825
ArchambaultP <ehras@hotmail.com>
2021-09-28 18:54:52 -0400

    first commit, modified setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ift6758',
+    packages=find_packages(),
+    # package_dir={'': 'src'},
+    version='0.1.0',
+    description='Sample project repo for IFT6758-2021',
+    author='Pascal Archambault, Jeremy Parent, Marcos Souto, Charles Collela',
+    license='',
+)
