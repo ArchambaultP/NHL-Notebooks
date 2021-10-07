32ceb499ce855a56f1ab7f284ed1d9b9363b0cc0
Jérémy Parent <djertnerap@live.ca>
2021-10-07 16:02:32 -0400

    Partial Data Acquisition Cleanup for blog post

--- ift6758/data/load_entire_dataset.py
+++ ift6758/data/load_entire_dataset.py
@@ -1,7 +1,7 @@
-from ift6758.data import import_dataset
+from ift6758.data import import_dataset, GameType
+
 
 def load_entire_dataset():
-    for season_start_year in [2016,2017, 2018, 2019, 2020]:
-        import_dataset(season_start_year,"R") # import data for the regular season
-        import_dataset(season_start_year, "P") # import playoff data
-load_entire_dataset()
+    for season_start_year in [2016, 2017, 2018, 2019, 2020]:
+        import_dataset(season_start_year, GameType.regular)  # import data for the regular season
+        import_dataset(season_start_year, GameType.playoffs)  # import playoff data
