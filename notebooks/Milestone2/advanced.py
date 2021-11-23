5cb7fe4a21d7bd2dccabcaaf27707f83cd192a88
msoutojr <msouto_jr@yahoo.com.br>
2021-11-22 22:25:34 -0500

    xgboost v2

--- notebooks/Milestone2/advanced.py
+++ notebooks/Milestone2/advanced.py
@@ -6,13 +6,13 @@ from ift6758.data import import_dataset
 from ift6758.features import tidy_data as td
 
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.preprocessing import LabelBinarizer
 
 import importlib
 import notebooks
 importlib.reload(notebooks)
 
 from notebooks.Milestone2.advanced_models import evaluate_xgb
-from notebooks.Milestone2.advanced_models import feature_selec_xgb
 from notebooks.Milestone2.Model import Model, Plots
 from notebooks.Milestone2.Model_XGB import Model_XGB
 
@@ -38,7 +38,7 @@ def one_hot(frame, feat, letter):
 
 def get_training_dataset() -> DataFrame:
     train_split_seasons = [2015, 2016, 2017, 2018]
-    #train_split_seasons = [2016]
+    #train_split_seasons = [2015]
     training_dataset = DataFrame()
     subset = DataFrame()
     for s in train_split_seasons:
@@ -50,12 +50,20 @@ def get_training_dataset() -> DataFrame:
             pdp2_tidied = td.tidy2_playbyplay_data(raw_data, pd.concat([pd.DataFrame(pbp_data),pdp_tidied], axis=1))
             training_dataset = training_dataset.append(pdp2_tidied)
             training_dataset.reset_index(drop=True, inplace=True)
+            print(str(s)+' '+str(game_type)) 
     
     training_dataset = training_dataset.drop(columns=['GamePk'])
-    training_dataset = training_dataset.dropna()
-    training_dataset.reset_index(drop=True, inplace=True)
-    training_dataset = one_hot(training_dataset, 'ShotType', 'f')
-    training_dataset = one_hot(training_dataset, 'Last_event_type', 'g')
+
+    lb = LabelBinarizer()
+    transfo = lb.fit_transform(training_dataset["Last_event_type"])
+    training_dataset = training_dataset.drop(columns=['Last_event_type'])
+    training_dataset[lb.classes_.astype(object)] = transfo
+
+    training_dataset.loc[training_dataset["ShotType"].isnull(), "ShotType"] = "Slap Shot"
+    transfo = lb.fit_transform(training_dataset["ShotType"])
+    training_dataset = training_dataset.drop(columns=['ShotType'])
+    training_dataset[lb.classes_.astype(object)] = transfo
+    
     
     return  training_dataset
 
